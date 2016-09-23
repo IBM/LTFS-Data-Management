5d212dcdcc2d1d4dc3cc2744b230005a790bf6df
map <map@zurich.ibm.com>
2016-09-23 21:28:29 +0200

    ... files were still missing

---
+++ src/common/util/util.cc
@@ -0,0 +1,39 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <string>
+
+#include "src/common/messages/Message.h"
+#include "src/common/tracing/Trace.h"
+#include "src/common/errors/errors.h"
+#include "src/common/const/Const.h"
+
+#include "util.h"
+
+
+void mkTmpDir()
+
+{
+	struct stat statbuf;
+
+	if ( stat(Const::LTFSDM_TMP_DIR.c_str(), &statbuf) != 0 ) {
+		if ( mkdir(Const::LTFSDM_TMP_DIR.c_str(), 0700) != 0 ) {
+			std::cerr << messages[LTFSDMX0006E] << Const::LTFSDM_TMP_DIR << std::endl;
+			throw(LTFSDMErr::LTFSDM_GENERAL_ERROR);
+		}
+	}
+	else if ( !S_ISDIR(statbuf.st_mode) ) {
+		std::cerr << Const::LTFSDM_TMP_DIR << messages[LTFSDMX0007E] << std::endl;
+		throw(LTFSDMErr::LTFSDM_GENERAL_ERROR);
+	}
+}
+
+
+void LTFSDM::init()
+
+{
+	mkTmpDir();
+	messageObject.init();
+	traceObject.init();
+}
---
+++ src/common/util/util.h
@@ -0,0 +1,8 @@
+#ifndef _UTIL_H
+#define _UTIL_H
+
+namespace LTFSDM {
+	void init();
+}
+
+#endif /*_UTIL_H */
